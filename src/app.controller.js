c5312911175d9a74f55a690ebafbfc9e9f9fe210
Ahmed Mohamed Omar <ahmedmohamedomar74@gmail.com>
2025-08-23 19:35:59 +0300

    feat: initialize project with Express and MongoDB connection
    - Added package.json with dependencies for Express, Mongoose, and other utilities.
    - Implemented database connection logic in connection.db.js using Mongoose.
    - Created basic database service functions for finding and creating documents in db.services.js.
    - Set up Express server in app.controller.js with environment variable configuration.
    - Bootstrapped application in index.js to start the server.

---
+++ src/app.controller.js
@@ -0,0 +1,22 @@
+import { testConnection } from "./DB/connection.db.js"
+import express from "express"
+import path from "node:path"
+import dotenv from "dotenv"
+
+
+
+async function bootstrap() {
+    dotenv.config({
+        path: path.resolve("./config/dev.env")
+    });
+    const port = process.env.PORT
+    const app = express()
+    // DB
+    testConnection()
+    app.use(express.json())
+    app.listen(port, () => {
+        console.log(`Server is running on port = ${port}`)
+    })
+}
+
+export default bootstrap
