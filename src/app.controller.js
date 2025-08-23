1ac99f2d372b9645ce1bf1e0d6c6c439b3d2dc2c
MMarzoo <mmarzouk12378@gmail.com>
2025-08-24 00:10:20 +0300

    add app and package json

--- src/app.controller.js
+++ src/app.controller.js
@@ -1,22 +1,22 @@
-import { testConnection } from "./DB/connection.db.js"
-import express from "express"
-import path from "node:path"
-import dotenv from "dotenv"
-
-
+import { testConnection } from "./DB/connection.db.js";
+import express from "express";
+import path from "node:path";
+import dotenv from "dotenv";
+import productRoute from "./modules/productRouter.js";
 
 async function bootstrap() {
-    dotenv.config({
-        path: path.resolve("./config/dev.env")
-    });
-    const port = process.env.PORT
-    const app = express()
-    // DB
-    testConnection()
-    app.use(express.json())
-    app.listen(port, () => {
-        console.log(`Server is running on port = ${port}`)
-    })
+  dotenv.config({
+    path: path.resolve("./config/dev.env"),
+  });
+  const port = process.env.PORT;
+  const app = express();
+  // DB
+  testConnection();
+  app.use(express.json());
+  app.use(productRoute);
+  app.listen(port, () => {
+    console.log(`Server is running on port = ${port}`);
+  });
 }
 
-export default bootstrap
+export default bootstrap;
