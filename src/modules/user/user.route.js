bebf0355eb9e53dd8b95dc1021caa89bf2f06062
Ahmed Mohamed Omar <ahmedmohamedomar74@gmail.com>
2025-08-26 22:03:14 +0300

    refactor: streamline authentication middleware usage in user routes

--- src/modules/user/user.route.js
+++ src/modules/user/user.route.js
@@ -3,10 +3,9 @@ import *as userServices from "./user.controller.js"
 import { auth, refreshAuth } from "../../middelwares/auth.middleware.js";
 import { removeUserCart } from "../../middelwares/cart.middelware.js";
 const router = Router()
-
-router.get("/",auth,userServices.profile)
-router.put("/",auth,userServices.updateProfile)
-router.get("/refresh-token",refreshAuth,userServices.profile)
-router.delete("/",auth,removeUserCart,userServices.deleteProfile)
+router.use(auth)
+router.get("/",userServices.profile)
+router.put("/",userServices.updateProfile)
+router.delete("/",removeUserCart,userServices.deleteProfile)
 
 export default router
