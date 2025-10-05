ea6df7f104da4a49cc8877659bddb8971970317e
Ahmed Mohamed Omar <ahmedmohamedomar74@gmail.com>
2025-10-06 00:55:17 +0300

    remove admin middelware from all category

--- src/modules/category/category.route.js
+++ src/modules/category/category.route.js
@@ -3,11 +3,11 @@ import { adminCheckmiddelware, auth } from "../../middelwares/auth.middleware.js
 import *as categoryServices from "./category.controller.js"
 
 const router = Router()
-router.use(auth,adminCheckmiddelware)
+router.use(auth)
 router.get("/",categoryServices.getAllCategory)
-router.post("/", categoryServices.createCategory);
-router.get("/:id",categoryServices.getCategory)
-router.put("/:id",categoryServices.updateCategory)
-router.delete("/:id",categoryServices.deleteCategory)
+router.post("/",adminCheckmiddelware, categoryServices.createCategory);
+router.get("/:id",adminCheckmiddelware,categoryServices.getCategory)
+router.put("/:id",adminCheckmiddelware,categoryServices.updateCategory)
+router.delete("/:id",adminCheckmiddelware,categoryServices.deleteCategory)
 
 export default router
